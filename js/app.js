8b4e835d369268d45f67bd291dc24cd25a78fa0c
gerardolongo <gerardo.longo1@gmail.com>
2016-07-04 17:39:04 +0200

    Add feedl accordion

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-var app = angular.module('ForecastApp', ['ngRoute','ngMessages','ngMaterial','ngResource','ngSanitize']);
+var app = angular.module('ForecastApp', ['ngRoute','ngMessages','ngMaterial','ngResource','ngSanitize','angAccordion']);
 
 
 app.config(function($routeProvider) {
